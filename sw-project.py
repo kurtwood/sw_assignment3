aa9aacb816a71297c9f277a32fe1ec28bfa8fd54
moviuro <echo bW92aXVybytpd2Fwcm9qZWN0QGdtYWlsLmNvbQo= | base64 -d>
2014-03-19 10:43:40 +0100

    New module on its way

---
+++ sw-project.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="moviuro"
+__date__ ="$Mar 19, 2014 10:42:48 AM$"
+
+if __name__ == "__main__":
+    print "Hello World"
