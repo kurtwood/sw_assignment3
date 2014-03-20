31a627bf62bbcabf319a98ea63cf30832b7f345a
moviuro <echo bW92aXVybytpd2Fwcm9qZWN0QGdtYWlsLmNvbQo= | base64 -d>
2014-03-20 13:18:18 +0100

    Added comments, began to set up for the next steps

--- sw-project.py
+++ sw-project.py
@@ -1,6 +1,13 @@
+# Import the SDK
 import facebook
+# import the secret token
 import secret
+# For date and time operations
+from datetime import datetime, date, time
 
+# open connection
 g = facebook.GraphAPI(secret.ACCESS_TOKEN)
 
+# retrieve friends
+friends = g.get_connections("me", "friends")['data']
 
