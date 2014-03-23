d438559d1aa0c1f84e09fa8b712d1d958f217573
moviuro <echo bW92aXVybytpd2Fwcm9qZWN0QGdtYWlsLmNvbQo= | base64 -d>
2014-03-23 15:03:02 +0100

    Now we get the number of likes and statuses per day for the last week

--- sw-project.py
+++ sw-project.py
@@ -31,3 +31,19 @@ statuses_times = []
 for friend in statuses:
     for status in statuses[friend]:
         statuses_times.append(datetime.strptime(status['updated_time'], fb_date_format))
+
+# Retrieve the number of Statuses per day for the last 7 days
+now = datetime.now()
+
+# How many...
+seconds_per_week = 7 * 24 * 60 * 60
+
+statuses_last_week = [0, 0, 0, 0, 0, 0, 0]
+for status_time in statuses_times:
+    if (now - status_time).total_seconds() < seconds_per_week:
+        statuses_last_week[status_time.weekday()] += 1
+
+likes_last_week = [0, 0, 0, 0, 0, 0, 0]
+for like_time in likes_times:
+    if (now - like_time).total_seconds() < seconds_per_week:
+        likes_last_week[like_time.weekday()] += 1
