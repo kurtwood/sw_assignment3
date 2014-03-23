a240cbaa13be8682e5611241634a761df581efff
moviuro <echo bW92aXVybytpd2Fwcm9qZWN0QGdtYWlsLmNvbQo= | base64 -d>
2014-03-23 15:05:53 +0100

    fix format

--- sw-project.py
+++ sw-project.py
@@ -12,19 +12,19 @@ g = facebook.GraphAPI(secret.ACCESS_TOKEN)
 friends = g.get_connections("me", "friends")['data']
 
 # retrieve their likes
-likes = { friend['name'] : g.get_connections(friend['id'], "likes")['data']
-        for friend in friends }
+likes = {friend['name']: g.get_connections(friend['id'], "likes")['data']
+         for friend in friends}
 
-statuses = { friend['name'] : g.get_connections(friend['id'], "statuses")['data']
-        for friend in friends }
+statuses = {friend['name']: g.get_connections(friend['id'], "statuses")['data']
+            for friend in friends}
 # take a look at a 'created_time' value of a random like, cf http://docs.python.org/2/library/datetime.html#strftime-and-strptime-behavior
 fb_date_format = "%Y-%m-%dT%H:%M:%S+0000"
 
 likes_times = []
 
 for friend in likes:
-  for like in likes[friend]:
-    likes_times.append(datetime.strptime(like['created_time'], fb_date_format))
+    for like in likes[friend]:
+        likes_times.append(datetime.strptime(like['created_time'], fb_date_format))
 
 statuses_times = []
 
